deca112038104575337987e02364805a69f7edbc
MADufour <47703233+bleeband@users.noreply.github.com>
2026-08-04 12:54:08 -0400

    fix(stage6): normalize roles and invalid approval responses

--- src/services/sensitiveTransitionService.js
+++ src/services/sensitiveTransitionService.js
@@ -7,6 +7,7 @@
 
 const db = require("../../db");
 const crypto = require("crypto");
+const UUID_RE = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
 const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 const isUuid = value => UUID_PATTERN.test(String(value || ""));
